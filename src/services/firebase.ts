1435b5fdb31285d9fc6d803e35d32e4253ba5f05
imanirvansyah <imanirvansyaah@gmail.com>
2025-02-01 11:41:19 +0700

    add env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -6,14 +6,14 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCHj2_fMghPNnhqttRyrOgpis76FfM6EGM",
-  authDomain: "portfolio-e50f6.firebaseapp.com",
-  databaseURL: "https://portfolio-e50f6-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "portfolio-e50f6",
-  storageBucket: "portfolio-e50f6.firebasestorage.app",
-  messagingSenderId: "976007183655",
-  appId: "1:976007183655:web:7361f932c5d20926b4bd02",
-  measurementId: "G-KRLTR87NFF"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
